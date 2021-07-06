c83e4a9eb86568f23c2abca4db89a28218d302b6
gillkyle <kylerobertgill@gmail.com>
2021-07-06 15:31:32 -0600

    add photos

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,16 +1,46 @@
 import * as React from "react";
-import { Box } from "@chakra-ui/react";
-import { StaticImage } from "gatsby-plugin-image";
+import { Box, Stack } from "@chakra-ui/react";
+import { StaticImage, GatsbyImage } from "gatsby-plugin-image";
+import { graphql } from "gatsby";
 
-const Photos = () => {
+const Photos = ({ data }) => {
+  console.log(data);
   return (
     <div>
       <title>Photos</title>
-      <Box display="flex" alignItems="center" maxWidth={320} margin="0 auto">
-        <StaticImage src="../images/engaged.jpg" />
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        maxWidth={320}
+        margin="0 auto"
+      >
+        {/* <Heading as="h2">Proposal</Heading> */}
+        <Box mb={4}>
+          <StaticImage src="../images/engaged.jpg" />
+        </Box>
+        <Stack>
+          {data.allFile.nodes.map((node) => (
+            <GatsbyImage image={node.childImageSharp.gatsbyImageData} />
+          ))}
+        </Stack>
       </Box>
     </div>
   );
 };
 
 export default Photos;
+
+export const query = graphql`
+  {
+    allFile(filter: { relativeDirectory: { regex: "/engagements/" } }) {
+      nodes {
+        id
+        relativeDirectory
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+`;
